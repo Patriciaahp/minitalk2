18d5739e19ab01af73c7fa6cca6d0669fac50a5c
Patriciaahp <patrihernandezperez9@gmail.com>
2025-07-03 19:11:40 +0200

    norminette

--- client.c
+++ client.c
@@ -1,82 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 17:36:50 by pahernan          #+#    #+#             */
+/*   Updated: 2025/07/03 17:36:50 by pahernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-int ft_atoi(const char *str)
+int	ft_atoi(char *str)
 {
-    int num;
-    int signe;
-    int negatif;
+	int	i;
+	int	j;
+	int	res;
 
-    negatif = 1;
-    signe = 0;
-    num = 0;
-    while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\n' || *str == '\f' || *str == '\r')
-        str++;
-    while (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            negatif = -1;
-        signe++;
-        if (signe > 1)
-            return (0);
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        num = num * 10 + (*str++ - '0');
-    }
-    return (num * negatif);
+	i = 0;
+	j = 0;
+	res = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			j++;
+		i++;
+	}
+	while ((str[i]) && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res = str[i] - '0' + res * 10;
+		i++;
+	}
+	if (j % 2 != 0)
+		res = -res;
+	return (res);
 }
 
-void send_signal(int pid, unsigned char character)
+void	send_signal(int pid, unsigned char character)
 {
-    unsigned char temp;
-    int i;
+	unsigned char	temp;
+	int				i;
 
-    i = 7;
-    temp = character;
-    while (i >= 0)
-    {
-        temp = character >> i;
-        if (temp % 2 == 0)
-            kill(pid, SIGUSR2);
-        else
-            kill(pid, SIGUSR1);
-        i--;
-        usleep(800);
-    }
+	i = 7;
+	temp = character;
+	while (i >= 0)
+	{
+		temp = character >> i;
+		if (temp % 2 == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		i--;
+		usleep(800);
+	}
 }
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    write(1, str, i);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	write(1, str, i);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    pid_t server_pid;
-    int i;
+	pid_t	server_pid;
+	int		i;
 
-    if (ac != 3)
-    {
-        ft_putstr("erreur pid str");
-        exit(0);
-    }
-    server_pid = ft_atoi(av[1]);
-    i = 0;
-    while (av[2][i])
-    {
-        send_signal(server_pid, (unsigned char)av[2][i]);
-        i++;
-    }
-    send_signal(server_pid, '\0');
-    return (0);
+	if (ac != 3)
+	{
+		ft_putstr("erreur pid str");
+		exit(0);
+	}
+	server_pid = ft_atoi(av[1]);
+	i = 0;
+	while (av[2][i])
+	{
+		send_signal(server_pid, (unsigned char)av[2][i]);
+		i++;
+	}
+	send_signal(server_pid, '\0');
+	return (0);
 }
